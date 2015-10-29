5f4f0cbcf44bf28a1f13c1e63e733c00cfa24181
huangm26 <huangm26@gmail.com>
2015-10-29 12:24:05 -0400

    Can get the process information, using the library from https://github.com/jaredrummler/AndroidProcesses

--- app/src/main/java/com/example/huangm26/monitorresource/MainActivity.java
+++ app/src/main/java/com/example/huangm26/monitorresource/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.huangm26.monitorresource;
 
+import android.app.ActivityManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.Iterator;
+import java.util.List;
+import library.src.main.java.com.jaredrummler.android.processes.ProcessManager;
+import library.src.main.java.com.jaredrummler.android.processes.models.AndroidAppProcess;
+import library.src.main.java.com.jaredrummler.android.processes.models.AndroidProcess;
+
+/**
+ * For the running process statistics, library from    https://github.com/jaredrummler/AndroidProcesses
+ */
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +27,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //Hide the title of App
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
 
+//        ActivityManager actvityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+//        List<ActivityManager.RunningAppProcessInfo> processList =  actvityManager.getRunningAppProcesses();
+        List<AndroidProcess> processList = ProcessManager.getRunningProcesses();
+        StringBuilder sb = new StringBuilder();
+        for (AndroidProcess process : processList) {
+            sb.append(process.name).append('\n');
+        }
+        Log.d("Process", "List size " + processList.size());
+//        Iterator iterator = processList.iterator();
+        Log.d("Process",sb.toString());
+//        while(iterator.hasNext())
+//        {
+//            ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo)iterator.next();
+//            Log.d("Process ","Process " + info.pid + " " + info.processName);
+//        }
+    }
+
+    private void setProcessList( List<ActivityManager.RunningAppProcessInfo> processList)
+    {
     }
 
     @Override
@@ -41,7 +75,10 @@ public class MainActivity extends AppCompatActivity {
         {
             case R.id.action_information:
                 break;
-
+            case R.id.action_setting:
+                break;
+            default:
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
